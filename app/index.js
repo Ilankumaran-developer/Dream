f11cafbb53fa2579befe5c81fb80f7bedec2cea5
ilandev <ilan.auctionsoftware@gmail.com>
2017-10-16 08:59:24 +0530

    ediii

--- app/index.js
+++ app/index.js
@@ -4,20 +4,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Register from './register.jsx';
+import NavigationMenu from './navigation.jsx';
 
-//import getMuiTheme from 'material-ui/styles/getMuiTheme';
-//import {blue500} from 'material-ui/styles/colors';
-//import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-//import injectTapEventPlugin from 'react-tap-event-plugin';
 
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-//injectTapEventPlugin();
-
-//todo: useragent is hardcoded temporariy, user routerContext to get instead or state props
 
 
 window.onload = () => {
     ReactDOM.render(
-        <Register/>, document.getElementById('app'));
+        <NavigationMenu/>, document.getElementById('app'));
 };
---
+++ app/navigation.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class NavigationMenu extends React.Component{
+
+render(){
+return(
+<div>
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div className="container">
+    <a className="navbar-brand" href="#">Tra Well</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarResponsive">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">Home
+            <span className="sr-only">(current)</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Register</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Login</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
+)
+}
+}
+
+export default NavigationMenu
